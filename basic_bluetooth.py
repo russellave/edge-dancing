573d72bdfd70715386c3556788e4a494b412e2d0
Brian Jordan <bjj17@duke.edu>
2020-02-27 15:32:46 -0500

    Fixed bug

--- basic_bluetooth.py
+++ basic_bluetooth.py
@@ -7,7 +7,7 @@ print "Connecting..."
 esp32 = btle.Peripheral("24:62:ab:d5:08:06")
 
 print "Services..."
-for svc in dev.services:
+for svc in esp32.services:
 	print str(svc)
 
 setup = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
