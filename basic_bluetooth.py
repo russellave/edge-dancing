b77326c849392f84beed8d823a8908298227af77
Brian Jordan <bjj17@duke.edu>
2020-02-27 15:28:09 -0500

    Reads from ble in a loop and prints results

--- basic_bluetooth.py
+++ basic_bluetooth.py
@@ -4,24 +4,23 @@ from bluepy import btle
 import binascii
 
 print "Connecting..."
-dev = btle.Peripheral("24:62:ab:d5:08:06")
+esp32 = btle.Peripheral("24:62:ab:d5:08:06")
 
 print "Services..."
 for svc in dev.services:
 	print str(svc)
 
-setupSensor = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+setup = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 
-setupService = dev.getServiceByUUID(setupSensor)
-
-
-readingSensor = btle.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
+setupService = esp32.getServiceByUUID(setup)
 
 
-setupConfig = setupService.getCharacteristics(readingSensor)[0]
+reading = btle.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
 
-val = setupConfig.read()
 
-# print "Light sensor raw value", binascii.b2a_hex(val)
+readingService = setupService.getCharacteristics(reading)[0]
 
-print "Light sensor raw value", val
+
+while True:
+	val = readingService.read()
+	print "Received Message: " + val
