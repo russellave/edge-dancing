59c8fc9ffc7b3383e4942bdb57ed60bc36b60c99
Brian Jordan <bjj17@duke.edu>
2020-02-26 23:07:32 -0500

    updates

--- basic_bluetooth.py
+++ basic_bluetooth.py
@@ -15,7 +15,7 @@ setupSensor = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 setupService = dev.getServiceByUUID(setupSensor)
 
 
-readingSensor = btle.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
+readingSensor = btle.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
 
 
 setupConfig = setupService.getCharacteristics(readingSensor)[0]
