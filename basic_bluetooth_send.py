a2fe1ba4233eb59cd4bcf3ee6f8a8c6db5b1f327
Brian Jordan <bjj17@duke.edu>
2020-02-27 15:40:43 -0500

    Seperated files for reading and writing to BLE

---
+++ basic_bluetooth_send.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+from bluepy import btle
+import binascii
+import time
+
+print "Connecting..."
+esp32 = btle.Peripheral("24:62:ab:d5:08:06")
+
+print "Services..."
+for svc in esp32.services:
+	print str(svc)
+
+setup = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+
+setupService = esp32.getServiceByUUID(setup)
+
+
+writing = btle.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
+
+
+writingService = setupService.getCharacteristics(writing)[0]
+
+
+while True:
+	writingService.write("A")
+	print "LED ON"
+
+	time.sleep(2)
+
+	writingService.write("B")
+	print "LED OFF"
+
+	time.sleep(2)
