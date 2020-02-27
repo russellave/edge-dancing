2384045c3e89e56b1927d04f71fbabc94f3081be
Brian Jordan <bjj17@duke.edu>
2020-02-26 23:05:17 -0500

    updates

--- basic_bluetooth.py
+++ basic_bluetooth.py
@@ -8,3 +8,17 @@ dev = btle.Peripheral("24:62:ab:d5:08:06")
 print "Services..."
 for svc in dev.services:
 	print str(svc)
+
+setupSensor = btle.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+
+setupService = dev.getServiceByUUID(setupSensor)
+
+
+readingSensor = btle.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
+
+
+setupConfig = setupService.getCharacteristics(readingSensor)[0]
+
+val = setupConfig.read()
+
+print "Light sensor raw value", binascii.b2a_hex(val)
